d140091d2a9257595d894fee79d73dc07b1f0bfe
“PedroFabian43“ <pmfabiangarcia@gmail.com>
2026-03-02 16:21:54 +0100

    He cambiado un extra

--- mainpracticaplantilla.py
+++ mainpracticaplantilla.py
@@ -1,8 +1,7 @@
-from services import servicepractica
-
+from services import servicepractica as serv
 
 print("-----Base de Datos de la Plantilla-----")
-service = servicepractica.ServicePlantilla()
+service = serv.ServicePlantilla()
 lista = service.getPlantilla()
 for empleado in lista:
     print(f"IdHos: {empleado.idHospital} \nSala: {empleado.idSala} \nNum.Empleado: {empleado.idEmpleado} \nNombre: {empleado.apellido} \nFunción: {empleado.funcion} \nTurno: {empleado.turno} \nSalario: {empleado.salario}")
